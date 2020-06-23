681618217b06b646ac4923146e137778a8a811f4
brevin <cronkbrevin98@gmail.com>
2020-06-22 18:07:04 -0700

    Shenanigigins

--- CoinCounter.js
+++ CoinCounter.js
@@ -1,41 +1,63 @@
 const num = 499;
 
-const Quarters = (qcounter) => {
+// function Coins(num) {
+// 	return function(quarts) {
+// 		return function(dims) {
+// 			return function(niks) {
+// 				return function(pens) {
+// 					return `Quarters: ${quarts}`;
+// 				};
+// 			};
+// 		};
+// 	};
+// }
+
+function Coins(qcounter) {
 	if (num - qcounter * 25 >= 25) {
-		Quarters(qcounter + 1);
+		return Coins(qcounter + 1);
 	} else {
-		console.log(qcounter);
-		Dimes(0);
+		console.log('Quarters: ', qcounter);
+		return function Coins(dcounter) {
+			if (num % 25 - dcounter * 10 >= 10) {
+				return Coins(dcounter + 1);
+			} else {
+				console.log('Dimes: ', dcounter);
+				return function Coins(ncounter) {
+					if (num % 10 - ncounter * 5 >= 5) {
+						return Coins(ncounter + 1);
+					} else {
+						console.log('Nickels: ', ncounter);
+						return function Coins(pcounter) {
+							if (num % 5 - pcounter >= 1) {
+								return Coins(pcounter + 1);
+							} else {
+								console.log(
+									'Quarters: ',
+									qcounter,
+									'Dimes: ',
+									dcounter,
+									'Nickels: ',
+									ncounter,
+									'Pennies: ',
+									pcounter
+								);
+							}
+						};
+					}
+				};
+			}
+		};
 	}
-};
+}
 
-const Dimes = (dcounter) => {
-	if (num % 25 - dcounter * 10 >= 10) {
-		Dimes(dcounter + 1);
-	} else {
-		console.log(dcounter);
-		Nickles(0);
-	}
-};
+const Dimes = Coins(0);
+const Nickels = Dimes(0);
+const Pennies = Nickels(0);
+Dimes(0);
+Nickels(0);
+Pennies(0);
 
-const Nickles = (ncounter) => {
-	if (num % 10 - ncounter * 5 >= 5) {
-		Nickles(ncounter + 1);
-	} else {
-		console.log(ncounter);
-		Pennies(0);
-	}
-};
-
-const Pennies = (pcounter) => {
-	if (num % 5 - pcounter >= 1) {
-		Pennies(pcounter + 1);
-	} else {
-		console.log(pcounter);
-	}
-};
-
-Quarters(0);
+// Coins(0)(0)(0)(0);
 
 // Nickles(0);
 // Pennies(0);
